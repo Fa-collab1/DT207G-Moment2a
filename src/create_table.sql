97ad4e67bc4a8bc21badd38b783af2d8ee0c2c61
Fa-collab1 <opus.ionam@gmail.com>
2024-04-06 15:40:03 +0200

    databask

--- src/create_table.sql
+++ src/create_table.sql
@@ -15,17 +15,17 @@ BEGIN
         -- Infogar exempeldata i den nyskapade tabellen
         INSERT INTO workexperience (companyname, jobtitle, location, startdate, enddate, description) 
         VALUES 
-            ('Telia', 'Billing Specialist', 'Solna', '2019-06-01', NULL, 'EDI and complex cases and also some development/automation work.'),
-            ('apotea.se', 'Systemutvecklare', 'Stockholm', '2018-07-01', '2018-12-31', NULL),
-            ('Plantvision AB', 'systemutvecklare', 'Stockholm', '2018-04-01', '2018-06-30', 'Praktik inom utbildning till systemutvecklare'),
-            ('Semantix AB', 'Group Business Controller', 'Stockholm', '2015-11-01', '2017-02-28', 'BI analytiker'),
-            ('Semantix AB', 'BI analytiker', 'Stockholm', '2009-08-01', '2015-11-30', 'BI business and procurement analyst'),
-            ('Semantix AB / TolkJouren', 'Tolkförmedlare / verksamhetsutvecklare / affärssystemskravställare', 'Stockholm', '2001-10-01', '2009-08-31', NULL),
-            ('Stockholms stad', 'Vårdbiträde', 'Stockholm', '2001-06-01', '2004-05-31', NULL),
-            ('Linköpings kommun', 'Vårdbiträde', 'Linköping', '2001-01-01', '2001-06-30', NULL),
-            ('Ericsson Radio Access AB - TDMA', 'Modellering och simulering', 'Stockholm', '2000-06-01', '2000-08-31', 'praktik inom civ.ing.utb.'),
-            ('Malungs kommun', 'Vårdbiträde/lärare', 'Malung', '1997-06-01', '2000-12-31', 'vikarie vid behov'),
-            ('Humle Pedagogik', 'Lärare i matematik', 'Transtrand', '1999-01-01', '1999-06-30', 'Komvux: Matematik B + C'),
-            ('Studieförbundet Vuxenskolan', 'Lärare i engelska', 'Malung', '1997-09-01', '1997-12-31', 'Komvux: Engelska B');
+            ('Telia Sverige AB', 'Billing Specialist', 'Solna, Sweden', '2019-06-01', NULL, 'EDI and complex cases and also some development/automation work.'),
+            ('apotea.se', 'System Developer', 'Stockholm, Sweden', '2018-07-01', '2018-12-31', NULL),
+            ('Plantvision AB', 'System Developer', 'Stockholm, Sweden', '2018-04-01', '2018-06-30', 'Internship in education for system developers'),
+            ('Semantix AB', 'Group Business Controller', 'Stockholm, Sweden', '2015-11-01', '2017-02-28', 'BI analyst'),
+            ('Semantix AB', 'BI Analyst', 'Stockholm, Sweden', '2009-08-01', '2015-11-30', 'BI business and procurement analyst'),
+            ('Semantix AB / TolkJouren', 'Interpreter Coordinator / Business Developer / Business System Requirement Specifier', 'Stockholm, Sweden', '2001-10-01', '2009-08-31', NULL),
+            ('Stockholm City', 'Healthcare Assistant', 'Stockholm, Sweden', '2001-06-01', '2004-05-31', 'Intermittant work while studying'),
+            ('Linköping Municipality', 'Healthcare Assistant', 'Linköping, Sweden', '2001-01-01', '2001-06-30', 'Intermittant work while studying'),
+            ('Ericsson Radio Access AB - TDMA', 'Modeling and Simulation', 'Stockholm, Sweden', '2000-06-01', '2000-08-31', 'Internship in civil engineering education'),
+            ('Malung Municipality', 'Healthcare Assistant/Teacher', 'Malung, Sweden', '1997-06-01', '2000-12-31', 'Intermittant work while studying'),
+            ('Humle Pedagogik', 'Mathematics Teacher', 'Transtrand, Sweden', '1999-01-01', '1999-06-30', 'Adult Education: Mathematics B + C'),
+            ('Studieförbundet Vuxenskolan', 'English Teacher', 'Malung, Sweden', '1997-09-01', '1997-12-31', 'Adult Education: English B');
     END IF;
 END $$;
